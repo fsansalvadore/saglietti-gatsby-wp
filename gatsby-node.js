e41ec1b3db090a1d84a46f07117493173f3d4757
fsansalvadore <f.sansalvadore@gmail.com>
2020-07-16 14:49:51 +0200

    setup graphql to query wp

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,75 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require(`path`)
 
-// You can delete this file if you're not using it
+const mediaFields = `
+  altText
+  uri
+`
+
+const seoFields = `
+  seo {
+    title
+    focuskw
+    metaDesc
+    metaKeywords
+    opengraphDescription
+    opengraphImage {
+      link
+    }
+    opengraphTitle
+    twitterDescription
+    twitterImage {
+      link
+    }
+    twitterTitle
+  }
+`
+
+const query = `
+  query {
+    wordpress {
+      posts(first: 100, where: { status: PUBLISH }) {
+        nodes {
+          categories {
+            nodes {
+              name
+              slug
+              termTaxonomyId
+            }
+          }
+          content
+          date
+          featuredImage {
+            node {
+              ${mediaFields}
+            }
+          }
+          ${seoFields}
+          status
+          uri
+          title
+        }
+      }
+    }
+  }
+`
+
+exports.createPages = async ({ actions, graphql }) => {
+  const { data } = await graphql(`
+    ${query}
+  `)
+  // console.log(data);
+  // if (!data.wordpress) return null
+
+  data.wordpress.posts.nodes.forEach(post => {
+    actions.createPage({
+      path: `/progetti${post.uri}`,
+      component: path.resolve(`./src/components/templates/project.jsx`),
+      context: {
+        ...post,
+        id: post.id,
+        slug: post.uri,
+        title: post.title
+      },
+    })
+  })
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,38 @@
 import React from "react"
-import { Link } from "gatsby"
+import { graphql, Link } from "gatsby"
 
 import Layout from "../components/layout"
 
-const IndexPage = () => (
+const IndexPage = ({data}) => (
   <Layout>
     <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-
+    {console.log(data)}
+    <hr/>
+    <h4>Posts:</h4>
+    <ul>
+      {data.wordpress.posts.nodes.map(post => (
+        <li>{post.date} - {post.slug}</li>
+      ))}
+    </ul>
     <Link to="/page-2/">Go to page 2</Link> <br />
     <Link to="/test/">Go to Test</Link> <br />
     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </Layout>
 )
+
+export const query = graphql`
+  query MyQuery {
+    wordpress {
+      posts {
+        nodes {
+          id
+          title
+          date
+          slug
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
