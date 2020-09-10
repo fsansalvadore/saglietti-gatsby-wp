cc813a354e1a60cb2583d60b7d681732b4dc4fbe
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2020-09-10 18:20:07 +0200

    mobile footer

--- src/components/organisms/footer/footer.component.jsx
+++ src/components/organisms/footer/footer.component.jsx
@@ -7,25 +7,41 @@ import SocialIcons from '../../molecules/SocialIcons/SocialIcons.component'
 
 const FooterComponent = styled.footer`
   width: 100%;
-  padding: 70px 2rem;
+  padding: 50px 1rem;
   display: flex;
+  flex-direction: column;
   border-top: 1px solid var(--line-color);
+  text-align: center;
+  align-items: center;
 
   .footer-logo {
-    width: 33%;
+    width: 25%;
+  }
+
+  div {
+    margin: 20px 0;
   }
 
   .footer-info {
-    width: 16.6%;
+    width: 100%;
     font-size: 0.8rem;
 
     p {
       margin: 0 0 0.5rem 0;
     }
+
+  }
+  .flex-end {
+    display: flex;
+    justify-content: center;
   }
 
   .footer-sitemap {
-    width: 16.6%;
+    width: 100%;
+
+    ul {
+      margin: 0;
+    }
 
     li {
       list-style: none;
@@ -36,6 +52,29 @@ const FooterComponent = styled.footer`
       }
     }
   }
+
+  @media only screen and (min-width: 900px) {
+    padding: 70px 2rem;
+    flex-direction: row;
+    text-align: left;
+    align-items: flex-start;
+
+    div {
+      margin: 0;
+    }
+
+    .footer-sitemap, .footer-info {
+      width: 16.6%;
+
+      &.large {
+        width: 24%;
+      }
+    }
+
+    .flex-end {
+      justify-content: flex-end;
+    }
+  }
 `
 
 const Footer = () => {
@@ -44,7 +83,7 @@ const Footer = () => {
       <div className="footer-logo">
         <img src={Logo} alt="Saglietti"/>
       </div>
-      <div className="footer-info">
+      <div className="footer-info large">
         <p>Copyright © {new Date().getFullYear()}</p>
         <p>P.I 03391740044</p>
         <p><a href="mailto:info@saglietti.it">info@saglietti.it</a></p>
@@ -60,7 +99,7 @@ const Footer = () => {
       <div className="footer-info">
         <Link to="/privacy">Privacy Policy</Link>
       </div>
-      <div className="footer-info">
+      <div className="footer-info flex-end">
         <SocialIcons/>
       </div>
     </FooterComponent>
