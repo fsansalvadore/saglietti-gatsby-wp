412ec1045b954a6fe341cd5109ad686e2f680d6d
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2020-09-21 11:32:17 +0200

    debugging gatsby-plugin-offline

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,10 +6,12 @@ export const wrapRootElement = ({ element }) => (
   <ApolloProvider client={client}>{element}</ApolloProvider>
 )
 
-// export const onClientEntry = () => {
-//   // IntersectionObserver polyfill for gatsby-background-image (Safari, IE)
-//   if (!(`IntersectionObserver` in window)) {
-//     import(`intersection-observer`)
-//     console.log(`# IntersectionObserver is polyfilled!`)
-//   }
-// }
+export const onServiceWorkerUpdateReady = () => {
+  const answer = window.confirm(
+    `This application has been updated. ` +
+      `Reload to display the latest version?`
+  )
+  if (answer === true) {
+    window.location.reload()
+  }
+}
--- gatsby-config.js
+++ gatsby-config.js
@@ -61,20 +61,12 @@ module.exports = {
         start_url: `/`,
         background_color: `#ffffff`,
         theme_color: `#000000`,
-        display: `minimal-ui`,
         icon: `src/images/favicon.svg`, // This path is relative to the root of the site.
         // crossOrigin: `use-credentials`,
       },
-    }
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-  //   {
-  //     resolve: 'gatsby-plugin-offline',
-  //     // options: {
-  //     //    workboxConfig: {
-  //     //       globPatterns: ['**/images*']
-  //     //    }
-  //     // }
-  //  }
+    'gatsby-plugin-offline'
   ],
 }
