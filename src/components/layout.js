63a871b2757ad723f4d08265cbca8ee20125849c
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2020-09-23 18:40:15 +0200

    cookie banner

--- src/components/layout.js
+++ src/components/layout.js
@@ -16,6 +16,7 @@ import Footer from "./organisms/footer/footer.component"
 import Cursor from "./atoms/cursor.component"
 import CursorFollow from "./atoms/cursor-follow.component"
 import GenericMetadata from './particles/meta/GenericMetadata'
+import { Cookies, CookiesProvider, CookieBannerUniversal } from 'react-cookie-banner'
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -29,6 +30,7 @@ const Layout = ({ children }) => {
   `)
   const [cursorComp, setCursorComp] = useState(null)
   const [cursorFollowComp, setCursorFollowComp] = useState(null)
+  const cookies = new Cookies(/* Your cookie header, on browsers defaults to document.cookie */)
 
   useEffect(() => {
     if ( typeof document !== `undefined` ) {
@@ -42,6 +44,56 @@ const Layout = ({ children }) => {
       <GenericMetadata/>
       {cursorComp}
       {cursorFollowComp}
+      <CookiesProvider cookies={cookies}>
+        <CookieBannerUniversal
+          className="cooky"
+          styles={{
+            banner: {
+              backgroundColor: '#fff',
+              minHeight: '100px',
+              height: 'auto',
+              bottom: 0,
+              zIndex: '1000',
+              width: '100vw',
+              display: 'flex',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+              padding: '30px',
+              borderTop: '1px solid #000',
+              boxSizing: 'border-box'
+            },
+            message: {
+              color: '#000',
+              maxWidth: '900px',
+              paddingRight: '30px',
+              width: '80vw',
+              display: 'block',
+              textAlign: 'left',
+              lineHeight: '140%',
+              fontWeight: '800'
+            },
+            button: {
+              cursor: 'pointer',
+              backgroundColor: '#000',
+              borderRadius: '0',
+              padding: '6px 12px',
+              color: '#fff',
+              fontWeight: '800',
+              fontSize: '16px',
+              height: 'auto',
+              top: '0',
+              position: 'relative',
+              float: 'right',
+              margin: 0,
+              right: 'none',
+            }
+          }}        
+          message="Questo sito fa uso di cookie per migliorare l’esperienza di navigazione degli utenti e per raccogliere informazioni sull’utilizzo del sito stesso. Proseguendo nella navigazione si accetta l’uso dei cookie."
+          onAccept={() => {}}
+          cookie="user-has-accepted-cookies"
+          buttonMessage="Accetto"
+        />
+      </CookiesProvider>
       <Nav siteTitle={data.site.siteMetadata.title} />
         <AnimatePresence exitBeforeEnter>
           <motion.main
