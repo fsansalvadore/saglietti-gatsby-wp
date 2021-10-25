c3567c6b01a760d22d1370eec550579b7e896eca
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2021-10-25 17:28:29 +0200

    feat(PEC): added pec email to contacts

--- src/components/organisms/contacts-info-container/contacts-info-container.component.jsx
+++ src/components/organisms/contacts-info-container/contacts-info-container.component.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import ContactForm from '../contact-form/contact-form.component'
+import React from "react"
+import ContactForm from "../contact-form/contact-form.component"
 
 const ContactsInfoContainer = () => {
   return (
@@ -9,23 +9,44 @@ const ContactsInfoContainer = () => {
           <div className="text-block half">
             <h2>New Business.</h2>
             <p>
-              Ogni nuovo progetto lancia una sfida. La tua qual è?<br/>
-              Siamo pronti ad ascoltare le tue esigenze e quelle del tuo brand.<br/>
+              Ogni nuovo progetto lancia una sfida. La tua qual è?
+              <br />
+              Siamo pronti ad ascoltare le tue esigenze e quelle del tuo brand.
+              <br />
               Sarà un piacere incontrarti.
             </p>
-            <a href="mailto:alessandro@saglietti.it" target="_blank" rel="noreferrer">alessandro@saglietti.it</a>
+            <a
+              href="mailto:alessandro@saglietti.it"
+              target="_blank"
+              rel="noreferrer"
+              className="tw-block tw-mb-20"
+            >
+              alessandro@saglietti.it
+            </a>
+            <a
+              href="mailto:alessandrosaglietti@pec.it"
+              target="_blank"
+              rel="noreferrer"
+              className="tw-block tw-mb-20"
+            >
+              alessandrosaglietti@pec.it
+            </a>
           </div>
           <div className="text-block half">
             <h2>Internship.</h2>
             <p>
-              Mandaci il tuo portfolio insieme a una breve presentazione.<br/>
-              Ti ricontatteremo appena possibile. Al momento l'organico è al completo.
+              Mandaci il tuo portfolio insieme a una breve presentazione.
+              <br />
+              Ti ricontatteremo appena possibile. Al momento l'organico è al
+              completo.
             </p>
-            <a href="mailto:info@saglietti.it" target="_blank" rel="noreferrer">info@saglietti.it</a>
+            <a href="mailto:info@saglietti.it" target="_blank" rel="noreferrer">
+              info@saglietti.it
+            </a>
           </div>
         </div>
       </div>
-      <hr className="line"/>
+      <hr className="line" />
       <div className="header-text-center flex align-center h-screen">
         <ContactForm />
       </div>
