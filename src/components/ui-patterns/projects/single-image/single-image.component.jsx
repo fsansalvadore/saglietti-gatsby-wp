af79909c10ea9c1330dca2b1d548d3295cca26db
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2025-07-13 23:34:33 +0200

    fix node issues

--- src/components/ui-patterns/projects/single-image/single-image.component.jsx
+++ src/components/ui-patterns/projects/single-image/single-image.component.jsx
@@ -1,6 +1,6 @@
 import React from "react"
-import styled from "styled-components"
 // import { Fade } from "react-awesome-reveal"
+import styled from "styled-components"
 
 const StyledImageContainer = styled.figure`
   position: relative;
